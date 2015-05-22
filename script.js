1313bc5716cad41980a529f6c9524fae896c5b02
Parker Lewis <parkerlewis@Parkers-MacBook-Air.local>
2015-05-21 17:17:19 -0700

    equal and clear button left

--- script.js
+++ script.js
@@ -9,12 +9,25 @@ $(function() {
 	var operator = ""
 	var firstHalf = ""
 	var total2 = ""
+
+	function reset() {
+		screenNum = "";
+		clickedNum = "";
+		numsArr1 = [];
+		numsArr2 = [];
+		total1 = ""
+		operator = ""
+		firstHalf = ""
+		total2 = ""
+	}
 	numberSpans.click(function(e) {
+//When you press a number and haven't yet pressed an operator
 		if(e.target.className === "" && firstHalf === "") {
 			 clickedNum = this.innerHTML;
 			 numsArr1.push(clickedNum);
 			 total1 = numsArr1.join("");
 			 $("#screen").html(total1);
+//If you press an operator thats not return or clear
 		} else if (e.target.className === "operator" && e.target.id === "") {
 			if(firstHalf === "") {
 				 firstHalf = total1
@@ -26,13 +39,23 @@ $(function() {
 				 console.log(firstHalf);
 				 console.log(operator)
 			} 
-
 		} else if (e.target.className === "" && firstHalf !== "") {
 			clickedNum = this.innerHTML;
 			numsArr2.push(clickedNum);
 			total2 = numsArr2.join("");
 			$("#screen").html(total1 + total2);
 			console.log(total2)
+		} else if (e.target.id = "calc") {
+			if(operator === "+") {
+				$("#screen").html(parseInt(total1) + parseInt(total2));
+				reset()
+
+				console.log(firstHalf)
+			} else if (operator === "-") {
+				$("#screen").html(parseInt(total1) - parseInt(total2));
+			} else if (operator === "*") {
+				$("#screen").html(parseInt(total1) * parseInt(total2));
+			} else if (operator === "&divide;")
 		}
 		//Equal button
 
